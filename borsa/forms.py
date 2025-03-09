83f2542ce5647463a7b3e493a8e7e07c23a1786e
ysfkcbnr <ysfgkcbnr@domain.com>
2025-03-10 00:35:00 +0300

    o

--- borsa/forms.py
+++ borsa/forms.py
@@ -8,6 +8,13 @@ from .models import UserProfile
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.models import User
 
+from django import forms
+
+class AddClassForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'})
 
 class AnalysisForm(forms.ModelForm):
     class Meta:
