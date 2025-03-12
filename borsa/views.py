405d5e8bf2b8c2254bdb6e49b96933a718fe5dd3
ysfkcbnr <ysfgkcbnr@domain.com>
2025-03-13 01:24:25 +0300

    BIST 100 hisseleri XU100 listesine eklendi

--- borsa/views.py
+++ borsa/views.py
@@ -86,8 +86,25 @@ def remove_from_tracking(request, hisse_id):
 
 def borsa_anasayfa(request):
     print("View çalışıyor aga!")
-    XU30_HISSELERI = ['GARAN', 'AKBNK', 'ISCTR']
-    XU100_HISSELERI = ['XU100', 'THYAO', 'GARAN', 'AKBNK', 'ISCTR']
+
+    # XU30 ve XU100 listeleri (senin eklemen gerekenler için şimdilik böyle)
+    XU30_HISSELERI = [
+        'AEFES', 'AKBNK', 'ALARK', 'ASELS', 'ASTOR', 'BIMAS', 'EKGYO', 'ENKAI', 'EREGL', 'FROTO',
+        'GARAN', 'HEKTS', 'ISCTR', 'KCHOL', 'KONTR', 'KOZAL', 'KRDMD', 'MGROS', 'PETKM', 'PGSUS',
+        'SAHOL', 'SASA', 'SISE', 'TCELL', 'THYAO', 'TOASO', 'TTKOM', 'TUPRS', 'ULKER', 'YKBNK'
+    ]
+    XU100_HISSELERI = [
+        'AEFES', 'AGHOL', 'AGROT', 'AKBNK', 'AKFYE', 'AKSA', 'AKSEN', 'ALARK', 'ALFAS', 'ALTNY',
+        'ANHYT', 'ANSGR', 'ARCLK', 'ARDYZ', 'ASELS', 'ASTOR', 'BERA', 'BIMAS', 'BRSAN', 'BRYAT',
+        'BSOKE', 'BTCIM', 'CANTE', 'CCOLA', 'CIMSA', 'CLEBI', 'CVKMD', 'CWENE', 'DOAS', 'DOHOL',
+        'ECILC', 'EGEEN', 'EKGYO', 'ENERY', 'ENJSA', 'ENKAI', 'EREGL', 'EUPWR', 'FENER', 'FROTO',
+        'GARAN', 'GESAN', 'GOLTS', 'GUBRF', 'HALKB', 'HEKTS', 'IEYHO', 'ISCTR', 'ISMEN', 'KARSN',
+        'KCAER', 'KCHOL', 'KLSER', 'KONTR', 'KONYA', 'KOZAA', 'KOZAL', 'KRDMD', 'LIDER', 'MAGEN',
+        'MAVI', 'MGROS', 'MIATK', 'MPARK', 'NTHOL', 'ODAS', 'OTKAR', 'OYAKC', 'PASEU', 'PETKM',
+        'PGSUS', 'REEDR', 'SAHOL', 'SASA', 'SDTTR', 'SELEC', 'SISE', 'SKBNK', 'SMRTG', 'SOKM',
+        'TABGD', 'TAVHL', 'TCELL', 'THYAO', 'TKFEN', 'TMSN', 'TOASO', 'TSKB', 'TSPOR', 'TTKOM',
+        'TTRAK', 'TUKAS', 'TUPRS', 'TURSG', 'ULKER', 'VAKBN', 'VESTL', 'YEOTK', 'YKBNK', 'ZOREN'
+    ]
 
     EXCHANGE_MAP = {
         'XIST': 'BIST',
@@ -95,13 +112,81 @@ def borsa_anasayfa(request):
         'NYQ': 'NYSE',
     }
 
-    try:
-        hisse_sembolleri = {
-            "XU100.IS": "XU100",
-            "GARAN.IS": "GARAN",
-            "THYAO.IS": "THYAO",
-        }
+    # Listenden gelen tüm hisse sembolleri (dinamik olarak eklenecek)
+    tum_hisseler = [
+        "A1CAP", "ACSEL", "ADEL", "ADESE", "ADGYO", "AEFES", "AFYON", "AGESA", "AGHOL", "AGROT",
+        "AGYO", "AHGAZ", "AHSGY", "AKBNK", "AKCNS", "AKENR", "AKFGY", "AKFIS", "AKFYE", "AKGRT",
+        "AKMGY", "AKSA", "AKSEN", "AKSGY", "AKSUE", "AKYHO", "ALARK", "ALBRK", "ALCAR", "ALCTL",
+        "ALFAS", "ALGYO", "ALKA", "ALKIM", "ALKLC", "ALMAD", "ALTNY", "ALVES", "ANELE", "ANGEN",
+        "ANHYT", "ANSGR", "APBDL", "APLIB", "APX30", "ARASE", "ARCLK", "ARDYZ", "ARENA", "ARMGD",
+        "ARSAN", "ARTMS", "ARZUM", "ASELS", "ASGYO", "ASTOR", "ASUZU", "ATAGY", "ATAKP", "ATATP",
+        "ATEKS", "ATLAS", "ATSYH", "AVGYO", "AVHOL", "AVOD", "AVPGY", "AVTUR", "AYCES", "AYDEM",
+        "AYEN", "AYES", "AYGAZ", "AZTEK", "BAGFS", "BAHKM", "BAKAB", "BALAT", "BALSU", "BANVT",
+        "BARMA", "BASCM", "BASGZ", "BAYRK", "BEGYO", "BERA", "BEYAZ", "BFREN", "BIENY", "BIGCH",
+        "BIGEN", "BIMAS", "BINBN", "BINHO", "BIOEN", "BIZIM", "BJKAS", "BLCYT", "BMSCH", "BMSTL",
+        "BNTAS", "BOBET", "BORLS", "BORSK", "BOSSA", "BRISA", "BRKO", "BRKSN", "BRKVY", "BRLSM",
+        "BRMEN", "BRSAN", "BRYAT", "BSOKE", "BTCIM", "BUCIM", "BULGS", "BURCE", "BURVA", "BVSAN",
+        "BYDNR", "CANTE", "CASA", "CATES", "CCOLA", "CELHA", "CEMAS", "CEMTS", "CEMZY", "CEOEM",
+        "CGCAM", "CIMSA", "CLEBI", "CMBTN", "CMENT", "CONSE", "COSMO", "CRDFA", "CRFSA", "CUSAN",
+        "CVKMD", "CWENE", "DAGHL", "DAGI", "DAPGM", "DARDL", "DCTTR", "DENGE", "DERHL", "DERIM",
+        "DESA", "DESPC", "DEVA", "DGATE", "DGGYO", "DGNMO", "DIRIT", "DITAS", "DMRGD", "DMSAS",
+        "DNISI", "DOAS", "DOBUR", "DOCO", "DOFER", "DOGUB", "DOHOL", "DOKTA", "DSTKF", "DURDO",
+        "DURKN", "DYOBY", "DZGYO", "EBEBK", "ECILC", "ECZYT", "EDATA", "EDIP", "EFORC", "EGEEN",
+        "EGEGY", "EGEPO", "EGGUB", "EGPRO", "EGSER", "EKGYO", "EKIZ", "EKOS", "EKSUN", "ELITE",
+        "EMKEL", "EMNIS", "ENDAE", "ENERY", "ENJSA", "ENKAI", "ENSRI", "ENTRA", "EPLAS", "ERBOS",
+        "ERCB", "EREGL", "ERSU", "ESCAR", "ESCOM", "ESEN", "ETILR", "ETYAT", "EUHOL", "EUKYO",
+        "EUPWR", "EUREN", "EUYO", "EYGYO", "FADE", "FENER", "FLAP", "FMIZP", "FONET", "FORMT",
+        "FORTE", "FRIGO", "FROTO", "FZLGY", "GARAN", "GARFA", "GEDIK", "GEDZA", "GENIL", "GENTS",
+        "GEREL", "GESAN", "GIPTA", "GLBMD", "GLCVY", "GLDTR", "GLRMK", "GLRYH", "GLYHO", "GMSTR",
+        "GMTAS", "GOKNR", "GOLTS", "GOODY", "GOZDE", "GRNYO", "GRSEL", "GRTHO", "GSDDE", "GSDHO",
+        "GSRAY", "GUBRF", "GUNDG", "GWIND", "GZNMI", "HALKB", "HALKS", "HATEK", "HATSN", "HDFGS",
+        "HEDEF", "HEKTS", "HKTM", "HLGYO", "HOROZ", "HRKET", "HTTBT", "HUBVC", "HUNER", "HURGZ",
+        "ICBCT", "ICUGS", "IDGYO", "IEYHO", "IHAAS", "IHEVA", "IHGZT", "IHLAS", "IHLGM", "IHYAY",
+        "IMASM", "INDES", "INFO", "INGRM", "INTEK", "INTEM", "INVEO", "INVES", "IPEKE", "ISATR",
+        "ISBIR", "ISBTR", "ISCTR", "ISDMR", "ISFIN", "ISGLK", "ISGSY", "ISGYO", "ISIST", "ISKPL",
+        "ISKUR", "ISMEN", "ISSEN", "ISYAT", "IZENR", "IZFAS", "IZINV", "IZMDC", "JANTS", "KAPLM",
+        "KAREL", "KARSN", "KARTN", "KARYE", "KATMR", "KAYSE", "KBORU", "KCAER", "KCHOL", "KENT",
+        "KERVN", "KERVT", "KFEIN", "KGYO", "KIMMR", "KLGYO", "KLKIM", "KLMSN", "KLNMA", "KLRHO",
+        "KLSER", "KLSYN", "KLYPV", "KMPUR", "KNFRT", "KOCMT", "KONKA", "KONTR", "KONYA", "KOPOL",
+        "KORDS", "KOTON", "KOZAA", "KOZAL", "KRDMA", "KRDMB", "KRDMD", "KRGYO", "KRONT", "KRPLS",
+        "KRSTL", "KRTEK", "KRVGD", "KSTUR", "KTLEV", "KTSKR", "KUTPO", "KUVVA", "KUYAS", "KZBGY",
+        "KZGYO", "LIDER", "LIDFA", "LILAK", "LINK", "LKMNH", "LMKDC", "LOGO", "LRSHO", "LUKSK",
+        "LYDHO", "LYDYE", "MAALT", "MACKO", "MAGEN", "MAKIM", "MAKTK", "MANAS", "MARBL", "MARKA",
+        "MARTI", "MAVI", "MEDTR", "MEGAP", "MEGMT", "MEKAG", "MEPET", "MERCN", "MERIT", "MERKO",
+        "METRO", "METUR", "MGROS", "MHRGY", "MIATK", "MMCAS", "MNDRS", "MNDTR", "MOBTL", "MOGAN",
+        "MOPAS", "MPARK", "MRGYO", "MRSHL", "MSGYO", "MTRKS", "MTRYO", "MZHLD", "NATEN", "NETAS",
+        "NIBAS", "NTGAZ", "NTHOL", "NUGYO", "NUHCM", "OBAMS", "OBASE", "ODAS", "ODINE", "OFSYM",
+        "ONCSM", "ONRYT", "OPT25", "OPTGY", "OPX30", "ORCAY", "ORGE", "ORMA", "OSMEN", "OSTIM",
+        "OTKAR", "OTTO", "OYAKC", "OYAYO", "OYLUM", "OYYAT", "OZATD", "OZGYO", "OZKGY", "OZRDN",
+        "OZSUB", "OZYSR", "PAGYO", "PAMEL", "PAPIL", "PARSN", "PASEU", "PATEK", "PCILT", "PEHOL",
+        "PEKGY", "PENGD", "PENTA", "PETKM", "PETUN", "PGSUS", "PINSU", "PKART", "PKENT", "PLTUR",
+        "PNLSN", "PNSUT", "POLHO", "POLTK", "PRDGS", "PRKAB", "PRKME", "PRZMA", "PSDTC", "PSGYO",
+        "QNBFK", "QNBTR", "QTEMZ", "QUAGR", "RALYH", "RAYSG", "REEDR", "RGYAS", "RNPOL", "RODRG",
+        "ROYAL", "RTALB", "RUBNS", "RYGYO", "RYSAS", "SAFKR", "SAHOL", "SAMAT", "SANEL", "SANFM",
+        "SANKO", "SARKY", "SASA", "SAYAS", "SDTTR", "SEGMN", "SEGYO", "SEKFK", "SEKUR", "SELEC",
+        "SELGD", "SELVA", "SERNT", "SEYKM", "SILVR", "SISE", "SKBNK", "SKTAS", "SKYLP", "SKYMD",
+        "SMART", "SMRTG", "SMRVA", "SNGYO", "SNICA", "SNKRN", "SNPAM", "SODSN", "SOKE", "SOKM",
+        "SONME", "SRVGY", "SUMAS", "SUNTK", "SURGY", "SUWEN", "TABGD", "TARKM", "TATEN", "TATGD",
+        "TAVHL", "TBORG", "TCELL", "TCKRC", "TDGYO", "TEKTU", "TERA", "TEZOL", "TGSAS", "THYAO",
+        "TKFEN", "TKNSA", "TLMAN", "TMPOL", "TMSN", "TNZTP", "TOASO", "TRCAS", "TRGYO", "TRILC",
+        "TSGYO", "TSKB", "TSPOR", "TTKOM", "TTRAK", "TUCLK", "TUKAS", "TUPRS", "TUREX", "TURGG",
+        "TURSG", "UFUK", "ULAS", "ULKER", "ULUFA", "ULUSE", "ULUUN", "UMPAS", "UNLU", "USAK",
+        "USDTR", "VAKBN", "VAKFN", "VAKKO", "VANGD", "VBTYZ", "VERTU", "VERUS", "VESBE", "VESTL",
+        "VKFYO", "VKGYO", "VKING", "VRGYO", "VSNMD", "X030S", "X100S", "XBANA", "XBANK", "XBLSM",
+        "XELKT", "XFINK", "XGIDA", "XGMYO", "XHARZ", "XHOLD", "XILTM", "XINSA", "XKAGT", "XKMYA",
+        "XKOBI", "XKURY", "XMADN", "XMANA", "XMESY", "XSADA", "XSANK", "XSANT", "XSBAL", "XSBUR",
+        "XSDNZ", "XSGRT", "XSIST", "XSIZM", "XSKAY", "XSKOC", "XSKON", "XSPOR", "XSTKR", "XTAST",
+        "XTCRT", "XTEKS", "XTM25", "XTMTU", "XTRZM", "XTUMY", "XU030", "XU050", "XU100", "XUHIZ",
+        "XULAS", "XUMAL", "XUSIN", "XUSRD", "XUTEK", "XUTUM", "XYLDZ", "XYORT", "XYUZO", "YAPRK",
+        "YATAS", "YAYLA", "YBTAS", "YEOTK", "YESIL", "YGGYO", "YGYO", "YIGIT", "YKBNK", "YKSLN",
+        "YONGA", "YUNSA", "YYAPI", "YYLGD", "Z30EA", "Z30KE", "Z30KP", "ZEDUR", "ZELOT", "ZGOLD",
+        "ZOREN", "ZPBDL", "ZPLIB", "ZPT10", "ZPX30", "ZRE20", "ZRGYO", "ZSR25", "ZTM25"
+    ]
 
+    # Dinamik olarak hisse_sembolleri sözlüğünü oluştur
+    hisse_sembolleri = {f"{hisse}.IS": hisse for hisse in tum_hisseler}
+
+    try:
         for sembol, isim in hisse_sembolleri.items():
             ticker = yf.Ticker(sembol)
             tarih_veri = ticker.history(period="2d")
@@ -123,7 +208,7 @@ def borsa_anasayfa(request):
                         'fiyat': round(bugunku_kapanis, 2),
                         'fiyat_degisim_yuzdesi': round(degisim_yuzdesi, 2),
                         'hacim': int(tarih_veri['Volume'].iloc[-1]),
-                        'is_bisttum': True,
+                        'is_bisttum': True,  # Hepsi BIST olduğu için True
                         'is_xu100': is_xu100,
                         'is_xu30': is_xu30,
                         'exchange': exchange,
@@ -139,6 +224,7 @@ def borsa_anasayfa(request):
                     hisse.exchange = exchange
                     hisse.save()
 
+        # Geri kalan kod aynı kalıyor...
         data = Hisse2.objects.all()
         arama = request.GET.get('arama', '').strip().upper()
         siralama = request.GET.get('siralama', 'isim')
@@ -212,7 +298,7 @@ def borsa_anasayfa(request):
     except Exception as e:
         print("Hata var aga:", str(e))
         data = []
-        arama = ''  # Varsayılan değer eklendi
+        arama = ''
         siralama = 'isim'
         kategori = 'TÜM HİSSELER'
         siralama_secenekleri = {
@@ -227,7 +313,7 @@ def borsa_anasayfa(request):
             'XU30': False,
             'XU100': False,
         }
-        borsa_kategorileri = []  # Varsayılan boş liste
+        borsa_kategorileri = []
 
     return render(request, 'index.html', {
         'data': data,
